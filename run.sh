0308f0e604278efec2a4034e8cbe06e2bfc67ad3
李响 <xiang.li@loodp.com>
2019-08-19 00:59:29 +0800

    no message

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#sh
+#!/bin/bash
 
 mv /etc/subversion/subversion-access-control /home/conf/subversion-access-control
 ln -s /home/conf/subversion-access-control /etc/subversion/subversion-access-control
