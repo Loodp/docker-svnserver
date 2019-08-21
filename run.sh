276467a985203e1645249d60043d4ee45d367590
李响 <xiang.li@loodp.com>
2019-08-21 22:22:39 +0800

    no message

--- run.sh
+++ run.sh
@@ -15,35 +15,5 @@ echo Running: "$@"
 # start svn
 /usr/bin/svnserve -d --foreground -r /home/svn --listen-port 3690
 
-###
-### Start apache...
-###
-
-if [[ `basename ${1}` == "httpd" ]]; then # prod
-# The tail approach...
-#
-# touch /var/log/apache2/error.log
-# touch /var/log/apache2/subversion.log
-# touch /var/log/apache2/access.log
-#
-# tail -f /var/log/apache2/error.log &
-# tail -f /var/log/apache2/subversion.log &
-# tail -f /var/log/apache2/access.log &
-
-# The direct approach...
-#
-ln -sf /dev/stderr /var/log/apache2/error.log
-ln -sf /dev/stdout /var/log/apache2/access.log
-ln -sf /dev/stdout /var/log/apache2/subversion.log
-
-exec "$@" </dev/null #>/dev/null 2>&1
-else # dev
-rm -f /var/log/apache2/error.log
-rm -f /var/log/apache2/access.log
-rm -f /var/log/apache2/subversion.log
-
-httpd -k start
-fi
-
 # fallthrough...
 exec "$@"
