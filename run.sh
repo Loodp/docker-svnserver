db75c05167e951fd335140259fdeec5278434f89
李响 <xiang.li@loodp.com>
2019-08-21 21:59:56 +0800

    no message

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 mv /etc/subversion/subversion-access-control /home/conf/subversion-access-control
 ln -s /home/conf/subversion-access-control /etc/subversion/subversion-access-control
 
-/usr/sbin/apachectl -DFOREGROUND
 /usr/bin/svnserve -d --foreground -r /home/svn --listen-port 3690
+exec /usr/sbin/apachectl -DFOREGROUND
