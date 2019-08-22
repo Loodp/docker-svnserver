75246cb647426e5d9bb4a5a76a5f911c3ce00a0f
李响 <xiang.li@loodp.com>
2019-08-22 21:16:35 +0800

    no message

--- run.sh
+++ run.sh
@@ -13,18 +13,11 @@ echo Running: "$@"
 ###
 ### Start apache...
 ###
-
-if [[ `basename ${1}` == "httpd" ]]; then # prod
-# The tail approach...
-
-exec "$@" </dev/null #>/dev/null 2>&1
-else # dev
 rm -f /var/log/apache2/error.log
 rm -f /var/log/apache2/access.log
 rm -f /var/log/apache2/subversion.log
 
 httpd -k start
-fi
 
 # fallthrough...
 exec "$@"
